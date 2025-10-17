5a6917f69752e356cc3c63f454e0d7053037cd95
Manveer Sohal <77809170+manveer-sohal@users.noreply.github.com>
2025-10-16 22:57:05 -0400

    added different catogries data

---
+++ codegories-backend/data/categories.js
@@ -0,0 +1,5 @@
+export const categories = {
+  "Data Structures": ["Stack", "Queue", "Array", "Graph"],
+  Algorithms: ["Merge Sort", "Dijkstra", "KMP", "DFS"],
+  "Programming Languages": ["Python", "C++", "Java", "Rust"],
+};
