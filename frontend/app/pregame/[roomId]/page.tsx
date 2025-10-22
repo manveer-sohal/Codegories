a3addf4d1356fbd66baff64615773b37ac9e2a1a
Manveer Sohal <77809170+manveer-sohal@users.noreply.github.com>
2025-10-22 15:19:47 -0400

    added rerouting to main page to prevent users to be in invalid rooms

--- frontend/app/pregame/[roomId]/page.tsx
+++ frontend/app/pregame/[roomId]/page.tsx
@@ -1,17 +1,33 @@
 "use client";
 import PreGameLobby from "@/components/PreGameLobby";
-import { joinRoom } from "@/lib/socket";
+import { checkRoomValid, joinRoom } from "@/lib/socket";
 import { useGameStore } from "@/lib/store";
 import { useParams, useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export default function PreGamePage() {
   const params = useParams<{ roomId: string }>();
+  const router = useRouter();
 
   useEffect(() => {
-    if (!params.roomId) return;
-    joinRoom(params.roomId, useGameStore.getState().playerName);
-  }, [params.roomId]);
+    if (!params.roomId) {
+      router.push("/");
+      return;
+    }
+    //check if the room is valid
+    checkRoomValid(params.roomId)
+      .then((valid: boolean) => {
+        if (!valid) {
+          router.push("/");
+          return;
+        }
+      })
+      .catch((error) => {
+        console.error("checkRoomValid error", error);
+        router.push("/");
+        return;
+      });
+  }, [params.roomId, router]);
 
   return (
     <div className="mx-auto max-w-xl mt-8 ">
