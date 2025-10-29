b2ca83d0b950c9699ac2c3ffcf3e949af95bfc88
Manveer Sohal <77809170+manveer-sohal@users.noreply.github.com>
2025-10-28 23:54:25 -0400

    updated playerlist styling

--- frontend/components/PlayerList.tsx
+++ frontend/components/PlayerList.tsx
@@ -3,11 +3,13 @@ import { useGameStore } from "@/lib/store";
 export default function PlayerList() {
   const players = useGameStore((s) => s.players);
   return (
-    <div>
-      <h2>Player List</h2>
+    <div className="bg-white/10 rounded-md p-2">
+      <div className="bg-white/10 rounded-md p-2">
+        <h2 className="text-white text-center">Player List</h2>
+      </div>
       {players.map((player) => (
         <div key={player.id}>
-          {player.name} {player.playerType}
+          {player.name} ({player.playerType})
         </div>
       ))}
     </div>
