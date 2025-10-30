f6c53ed8525d73a3f974c64b7b8a5b6fadb1079e
Manveer Sohal <77809170+manveer-sohal@users.noreply.github.com>
2025-10-30 05:08:22 -0400

    fixed scoreboard dispaly and reseting game for replay

--- frontend/app/results/page.tsx
+++ frontend/app/results/page.tsx
@@ -2,10 +2,32 @@
 
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import Scoreboard from "@/components/Scoreboard";
+import { resetGame } from "@/lib/socket";
+import { useGameStore } from "@/lib/store";
 import { useRouter } from "next/navigation";
-
+import { useEffect } from "react";
 export default function ResultsPage() {
+  const roomId = useGameStore((s) => s.roomId);
+  const phase = useGameStore((s) => s.phase);
   const router = useRouter();
+
+  useEffect(() => {
+    if (phase == "lobby") {
+      router.push(`/game/${roomId}`);
+    }
+  }, [phase, router, roomId]);
+  const handleResetGame = async () => {
+    const success = await resetGame(roomId!);
+    console.log("phase", phase);
+    if (success) {
+      router.push(`/game/${roomId}`);
+      console.log("Game reset successfully");
+    } else {
+      console.error("Failed to reset game");
+      alert("Failed to reset game");
+    }
+  };
 
   return (
     <div className="mx-auto max-w-2xl space-y-4">
@@ -15,12 +37,13 @@ export default function ResultsPage() {
         </CardHeader>
         <CardContent>
           <p className="text-white/70 text-sm">
-            Results summary will appear here.
+            Results:
+            <Scoreboard />
           </p>
         </CardContent>
       </Card>
       <div className="flex justify-end">
-        <Button onClick={() => router.push("/")}>Play Again</Button>
+        <Button onClick={handleResetGame}>Play Again</Button>
       </div>
     </div>
   );
