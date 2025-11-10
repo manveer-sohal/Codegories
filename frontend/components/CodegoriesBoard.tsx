c5660476a1c1c33a876a261270543a452c35bc9e
Manveer Sohal <77809170+manveer-sohal@users.noreply.github.com>
2025-11-10 17:37:44 -0500

    Add Codegories and CodegoriesBoard components for game functionality, including player input and scoring logic

---
+++ frontend/components/CodegoriesBoard.tsx
@@ -0,0 +1,84 @@
+import { useGameStore } from "@/lib/store";
+import { useState } from "react";
+import { Button } from "./ui/button";
+import {
+  DataStructures,
+  Algorithms,
+  ProgrammingLanguages,
+  TechCompanies,
+  CSBuzzwords,
+  ProgrammingKeywords,
+  Frameworks,
+} from "@/app/data/Data";
+
+export default function CodegoriesBoard({ letter }: { letter: string }) {
+  // const currentRound = useGameStore((s) => s.currentRound);
+  // const codegoriesRoundData = useGameStore((s) => s.codegoriesRoundData);
+  const [answers, setAnswers] = useState<{ [key: string]: string }>({});
+  // const categories = codegoriesRoundData?.categories ?? [];
+  const [submitted, setSubmitted] = useState<boolean>(false);
+  const [score, setScore] = useState<number>(0);
+  const categories = [
+    "Data Structure",
+    "Algorithm",
+    "Programming Language",
+    "CS Buzzword / Jargon",
+    "Programming Keyword",
+    "Tech Company",
+    "Framework",
+  ];
+  const submitCategories = () => {
+    setSubmitted(true);
+    let score = 0;
+    if (DataStructures[letter].has(answers["Data Structure"])) {
+      score++;
+    }
+    if (Algorithms[letter].has(answers["Algorithm"])) {
+      score++;
+    }
+    if (ProgrammingLanguages[letter].has(answers["Programming Language"])) {
+      score++;
+    }
+
+    if (TechCompanies[letter].has(answers["Tech Company"])) {
+      score++;
+    }
+    if (CSBuzzwords[letter].has(answers["CS Buzzword / Jargon"])) {
+      score++;
+    }
+    if (ProgrammingKeywords[letter].has(answers["Programming Keyword"])) {
+      score++;
+    }
+    if (Frameworks[letter].has(answers["Framework"])) {
+      score++;
+    }
+    setScore(score);
+  };
+
+  return (
+    <>
+      {categories.map((category) => (
+        <div key={category} className=" w-full flex flex-row gap-2">
+          <div className="flex h-11 w-50 items-center gap-2 bg-white/10 rounded-md">
+            <p className="px-2 w-full text-center">{category}</p>
+          </div>
+          <input
+            disabled={submitted}
+            type="text"
+            className="w-full h-10 bg-white/30 rounded-md text-white p-2"
+            onChange={(e) =>
+              setAnswers({ ...answers, [category]: e.target.value })
+            }
+          />
+        </div>
+      ))}
+      <Button
+        disabled={submitted}
+        className="w-full"
+        onClick={() => submitCategories()}
+      >
+        Submit
+      </Button>
+    </>
+  );
+}
