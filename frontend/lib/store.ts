783fcffe4ae95125765e6f500c892be62d3c30ca
Manveer Sohal <77809170+manveer-sohal@users.noreply.github.com>
2025-11-13 19:39:33 -0500

    Refactor GameState and Player management in store: replace PlayerScore array with a Map for players, add functions for adding/removing players, and update score handling methods for improved state management.

--- frontend/lib/store.ts
+++ frontend/lib/store.ts
@@ -13,11 +13,6 @@ export interface Player {
   playerType: "Host" | "Player";
   wins: number;
   avatar: string;
-}
-
-export interface PlayerScore {
-  playerId: string;
-  name: string;
   score: number;
 }
 
@@ -51,24 +46,27 @@ export interface GameState {
   round: number;
   // duration: number;
   currentRound?: RoundData;
-  scores: PlayerScore[];
-  players: Player[];
+  // socketId -> Player
+  players: Map<string, Player>;
   lastSubmittedAnswer?: string;
   playerCount: number;
   playerInput: string[];
   setGame: (game: "codegories" | "speedstorm" | "trivia") => void;
   setPlayerInput: (input: string[]) => void;
   setPlayerName: (playerName: string) => void;
-  setRoomId: (roomId: string) => void;
-  setPlayers: (players: Player[]) => void;
+  setRoomId: (roomId: string | undefined) => void;
+  addPlayer: (player: Player) => void;
+  removePlayer: (playerId: string) => void;
   setPhase: (phase: GamePhase) => void;
   setRound: (round: number) => void;
+  setPlayersList: (players: Map<string, Player>) => void;
   // setDuration: (duration: number) => void;
   setCurrentRound: (round: RoundData | undefined) => void;
   updateTimeRemaining: (seconds: number) => void;
-  setScores: (scores: PlayerScore[]) => void;
-  incrementScore: (playerId: string, delta: number) => void;
+  updateScore: (playerId: string, score: number) => void;
+  updateUserScore: (score: number) => void;
   setLastSubmittedAnswer: (answer?: string) => void;
+  updateWinner: (playerId: string) => void;
   reset: () => void;
 }
 
@@ -80,51 +78,106 @@ export const useGameStore = create<GameState>((set, get) => ({
   playerName: "",
   phase: "None",
   wins: 0,
+  roomId: undefined,
   score: 0,
   round: 1,
   // duration: 30,
-  scores: [],
   playerCount: 0,
-  players: [],
+  players: new Map<string, Player>(),
+
   playerInput: [],
+
+  updateWinner: (playerId: string) =>
+    set((state) => {
+      const player = state.players.get(playerId);
+
+      if (!player) return { players: state.players };
+
+      const updatedPlayer = { ...player, wins: player.wins + 1 };
+
+      state.players.set(playerId, updatedPlayer);
+
+      return { players: state.players };
+    }),
+
   setGame: (game: "codegories" | "speedstorm" | "trivia") => set({ game }),
+
   setPlayerType: (playerType: "None" | "Host" | "Player") =>
     set({ playerType }),
+
   setPlayerInput: (input: string[]) => set({ playerInput: input }),
-  getPlayers: () => get().players,
-  setPlayers: (players: Player[]) => set({ players }),
+
+  setPlayersList: (players: Map<string, Player>) => set({ players: players }),
+
+  // add a player to the players map
+  addPlayer: (player: Player) =>
+    set((state) => ({ players: state.players.set(player.id, player) })),
+
+  // remove a player from the players map and return the updated players map for the new state
+  removePlayer: (playerId: string) =>
+    set((state) => {
+      state.players.delete(playerId);
+      return { players: state.players };
+    }),
+
   setPlayerCount: (count: number) => set({ playerCount: count }),
+
   setRoomId: (roomId) => set({ roomId }),
+
   setPhase: (phase) => set({ phase }),
+
   setRound: (round) => set({ round }),
+
   setWins: (wins: number) => set({ wins }),
   // setDuration: (duration) => set({ duration }),
   setCurrentRound: (currentRound) => set({ currentRound }),
+
   updateTimeRemaining: (seconds) =>
     set((state) =>
       state.currentRound
         ? { currentRound: { ...state.currentRound, timeRemaining: seconds } }
         : {}
     ),
-  setScores: (scores) => set({ scores }),
+
+  //update score of user
+  updateUserScore: (score: number) =>
+    set((state) => {
+      return { score: state.score + score };
+    }),
+
+  //update the score of a player
+  updateScore: (playerId: string, newScore: number) =>
+    set((state) => {
+      const player = state.players.get(playerId);
+
+      if (!player) return { players: state.players };
+
+      const updatedPlayer = { ...player, score: newScore };
+
+      state.players.set(playerId, updatedPlayer);
+
+      return { players: state.players };
+    }),
+
   getPlayerName: () => get().playerName,
   setPlayerName: (name: string) => set({ playerName: name }),
-  incrementScore: (playerId, delta) =>
-    set((state) => ({
-      scores: state.scores.map((s) =>
-        s.playerId === playerId ? { ...s, score: s.score + delta } : s
-      ),
-    })),
+
   setLastSubmittedAnswer: (answer) => set({ lastSubmittedAnswer: answer }),
+
   reset: () =>
     set({
       playerId: undefined,
       roomId: undefined,
-      phase: "lobby",
+      phase: "None",
+      playerType: "None",
+      playerName: "",
+      players: new Map<string, Player>(),
+      playerCount: 0,
+      playerInput: [],
       score: 0,
       round: 1,
       currentRound: undefined,
-      scores: [],
       lastSubmittedAnswer: undefined,
+      game: "codegories",
     }),
 }));
